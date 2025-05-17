db4ea60847c3281cf48f652b8a71b1d1b896f289
Edward Chia <edwardc.jianken@gmail.com>
2025-05-17 11:16:53 +0800

    Continue prev fix

--- react/grocery-store-catalogue/vite.config.js
+++ react/grocery-store-catalogue/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/code-playground",
+  base: "/code-playground/",
   plugins: [react()],
 });
