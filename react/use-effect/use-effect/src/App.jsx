6d336ad0fcfd6267a2b3ebc266bef35fa2fdc725
Edward Chia <edwardc.jianken@gmail.com>
2025-07-18 22:11:46 +0800

    Added practice code for use-effect.

---
+++ react/use-effect/use-effect/src/App.jsx
@@ -0,0 +1,11 @@
+import UseEffect from "./components/UseEffect/UseEffect";
+
+function App() {
+  return (
+    <>
+      <UseEffect />
+    </>
+  );
+}
+
+export default App;
---
+++ react/use-effect/use-effect/src/components/UseEffect/UseEffect.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+function UseEffect() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    console.log("EVENT LISTENER ADDED");
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      console.log("EVENT LISTENER REMOVED");
+    };
+  }, []);
+
+  useEffect(() => {
+    document.title = `size: ${width} x ${height}`;
+  }, [width, height]);
+
+  function handleResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+
+  return (
+    <>
+      <p>Window Width: {width}px</p>
+      <p>Window Width: {height}px</p>
+    </>
+  );
+}
+
+export default UseEffect;
