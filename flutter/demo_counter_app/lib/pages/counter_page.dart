5b44935bb449d0e19aa55ea0558e36630d2b239f
Edward Chia <edwardc.jianken@gmail.com>
2025-12-23 22:28:35 +0800

    Added practice code for flutter (counter-app)

---
+++ flutter/demo_counter_app/lib/pages/counter_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  // varable
+  int _counter = 0;
+
+  // method
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  // UI
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("You pushed this button this many times:"),
+            Text(_counter.toString(), style: TextStyle(fontSize: 40)),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: Text("Click here!"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
